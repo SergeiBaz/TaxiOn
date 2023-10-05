d195452a5da97794543edc5ea9668e32c64a543a
SergeiBaz <125749715+SergeiBaz@users.noreply.github.com>
2023-10-05 13:59:09 +0300

    completed view the auction for the driver(доделал view аукциона для водителя)

--- app/src/main/java/presentation/fragments/trip_fragment/AuctionPassengerFragment.kt
+++ app/src/main/java/presentation/fragments/trip_fragment/AuctionPassengerFragment.kt
@@ -3,4 +3,4 @@ package presentation.fragments.trip_fragment
 import androidx.fragment.app.Fragment
 import com.example.taxion.R
 
-class PassengerTripFragment : Fragment(R.layout.fragment_passenger_trip)
+class AuctionPassengerFragment : Fragment(R.layout.fragment_auction_passenger)
